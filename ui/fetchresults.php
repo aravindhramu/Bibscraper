d538e00ce557a7f1c2d119734e97492349b8174b
Aravindh <aravindhramu@gmail.com>
2012-08-31 15:51:55 +0530

    fixed bugs in ui

--- ui/fetchresults.php
+++ ui/fetchresults.php
@@ -1,14 +1,11 @@
 <?php
-
+$result = "";
 $isbn = $_GET['isbn'];
-print $isbn;
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL,"http://localhost:6800/schedule.json");
 curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
 curl_setopt($ch, CURLOPT_POSTFIELDS,"project=scrapBib&spider=flipkartspider&isbn=$isbn");
 $op1=json_decode(curl_exec($ch));
-if($op1->status == "ok")
-  print("ok");
 curl_close ($ch);
 
 $ch = curl_init();
@@ -25,5 +22,28 @@ curl_setopt($ch, CURLOPT_POSTFIELDS,"project=scrapBib&spider=homeshopspider&isbn
 $op3=json_decode(curl_exec($ch));
 curl_close ($ch); 
 
+
+sleep(5);
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL,"http://localhost:6800/listjobs.json");
+curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
+curl_setopt($ch, CURLOPT_POSTFIELDS,"project=scrapBib");
+$op=json_decode(curl_exec($ch),true);
+curl_close ($ch); 
+while(empty($op["jobs"])==false)
+{
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL,"http://localhost:6800/listjobs.json");
+curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
+curl_setopt($ch, CURLOPT_POSTFIELDS,"project=scrapBib");
+$op=json_decode(curl_exec($ch),true);
+curl_close ($ch); 
+}
+sleep(8);
+$result = file_get_contents('results.out');
+
+print $result;
+
+file_put_contents("results.out","");
 ?>
 
